225962934d67fe3be22c48b05bfd2da305635c2d
Roman Doroshenko <roman.doroshenko.tlt@gmail.com>
2017-07-10 10:29:24 +0400

    #4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 
+const style = {
+  backgroundColor: '#222',
+  color: '#ddd',
+  fontFamily: 'sans-serif',
+  border: "3px solid #555",
+  textAlign: "center"
+}
 
-const title = React.createElement('h1',{id: 'title',className: 'header'},'Hello World');
 
-ReactDOM.render(title, document.getElementById('react-container'));
+// const title = React.createElement(
+//   'h1',
+//   { id: 'title', className: 'header', style: style },
+//   'Hello World'
+// );
+
+ReactDOM.render(
+  <h1 id='title'
+      className='header'
+      style={style}>
+  Hello there!!!
+  </h1>,
+  document.getElementById('react-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
     path: path.join(__dirname,'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015','react']
+          }
+        }
+      }
+    ]
+  },
   devServer: {
     contentBase: path.join(__dirname,'dist'),
     port: 3000
